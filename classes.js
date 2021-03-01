e3746ac98d2fdf08aab4d74af572f68dac49f38d
Chad Basinger <chad.basingeris@gmail.com>
2021-03-01 15:23:54 -0700

    finished classes

--- classes.js
+++ classes.js
@@ -30,6 +30,20 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+
+  }
+
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+
 
 
 ////////// PROBLEM 2 //////////
@@ -48,7 +62,21 @@
 */
 
 //Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.reports = [];
+  }
 
+  hire(employee){
+    // this.employee = employee;
+    this.reports.push(employee)
+  }
+
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -72,7 +100,40 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age, reports)
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
 
+  hire(employee){
+    super.hire(employee)
+    if(this.reports.length === 0){
+      this.title = 'Not a manager'
+    }
+    if(this.reports.length > 0 && this.reports.length < 4){
+      this.title = 'Barely Manager'
+    }
+    if(this.reports.length > 3 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    }
+    if(this.reports.length > 10 && this.reports.length <= 50){
+      this.title = 'Manager'
+    }
+    if(this.reports.length > 50 && this.reports.length <= 100){
+      this.title = 'Manager Plus'
+    }
+    if(this.reports.length > 100){
+      this.title = 'Bestest Manager'
+    }
+  }
+
+  fire(index){
+    super.fire(index)
+    this.bonus += 100;
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -99,5 +160,39 @@
 */
 
 //Code Here
+class Machine {
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  
 
+  // makeWidgets(number){
+  //   this.widgets_made_count += number;
+  //   if(Math.trunc((this.widgets_made_count / 50)) >= 1){
+  //     this.wear_and_tear_count += Math.trunc((this.widgets_made_count / 50))
+  //   }
+  // }
+
+  makeWidgets(number){
+    this.widgets_made_count += number;
+    this.wear_and_tear_count = Math.floor((this.widgets_made_count / 50))
+
+  }
+
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+
+  reboot(){
+    if(this.needs_reboot === true){
+      return () => {
+        this.wear_and_tear_count -= 10;
+        this.needs_reboot = false;
+      }
+    }
+    
+  }
+}
 
